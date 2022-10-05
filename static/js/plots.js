32e3e56caed85d32f29ebab06796eeb9aaaded5a
Mark <markosowski2023@u.northwestern.edu>
2022-10-05 10:32:50 -0500

    tests

--- static/js/plots.js
+++ static/js/plots.js
@@ -6,3 +6,14 @@ var dataset = [];
 d3.csv(url, function(data) {
     console.log(data);
 })
+
+
+d3.csv(url, function (err, rows) {
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+    }
+
+    // we need to do data['company_location'].unique() in the ipynb and then loook up up the lat and long for each unique value and add coloums to the csv
+    var country = unpack(rows, 'company_location'),
+        remoteRatio = unpack(rows, 'remote_ratio')
+})
