c08510927ef806aa373f1cb18a4f32d004590a4e
Mark <markosowski2023@u.northwestern.edu>
2022-10-05 21:22:59 -0500

    data is in

--- resources/app.py
+++ resources/app.py
@@ -1,19 +1,81 @@
-import psycopg2
-from flask import Flask
-from flask import request
-from flask import jsonify
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+engine = create_engine("sqlite:///ds_salaries.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+salaries = Base.classes.salaries
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
-app.config["DEBUG"] = True
-@app.route('/api/v1/salaries/all', methods=['GET'])
-def apiViewAll():
-    con = psycopg2.connect(host='localhost', dbname='DataScienceSalaries', user='postgres', password='postG')
-    cur = con.cursor()
-    all_salaries = cur.execute('SELECT * FROM salaries').fetchall()
-    cur.close()
-    return jsonify(all_salaries)
+CORS(app)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/all<br/>"
+        f"/api/v1.0/searchbyjobtitle/<job_title><br/>"
+        f"/api/v1.0/searchbysalaryinusd/<salary_in_usd><br/>"
+        f"/api/v1.0/searchbyremoteratio/<remote_ratio><br/>"
+    )
 
 
+@app.route("/api/v1.0/all")
+def view_all():
+    session = Session(engine)
+    results = session.query(salaries).all()
+    #output = [{"job_title": x.job_title, "salary_in_usd": x.salary_in_usd} for x in results]
+    output = [{"id": x.id, "work_year": x.work_year, "experience_level": x.experience_level, "job_title": x.job_title, "salary": x.salary, "salary_currency": x.salary_currency, "salary_in_usd": x.salary_in_usd, "employee_residence": x.employee_residence,
+    "remote_ratio": x.remote_ratio, "company_location": x.company_location, "company_size": x.company_size, "country_name": x.country_name} for x in results]
+    session.close()
+    return jsonify(output)
 
-if __name__ == "__main__":
+
+@app.route("/api/v1.0/searchbyjobtitle/<job_title>")
+def search_by_job_title(job_title):
+    session = Session(engine)
+    results = session.query(salaries).filter(salaries.job_title==job_title).all()
+    output = [{"job_title": x.job_title, "salary_in_usd": x.salary_in_usd} for x in results]
+    session.close()
+    return jsonify(output)
+
+
+@app.route("/api/v1.0/searchbysalaryinusd/<salary_in_usd>")
+def search_by_salary_in_usd(salary_in_usd):
+    session = Session(engine)
+    results = session.query(salaries).filter(salaries.salary_in_usd==salary_in_usd).all()
+    output = [{"job_title": x.job_title, "salary_in_usd": x.salary_in_usd} for x in results]
+    session.close()
+    return jsonify(output)
+
+@app.route("/api/v1.0/searchbyremoteratio/<remote_ratio>")
+def search_by_remote_ratio(remote_ratio):
+    session = Session(engine)
+    results = session.query(salaries).filter(salaries.remote_ratio==remote_ratio).all()
+    output = [{"job_title": x.job_title, "remote_ratio": x.remote_ratio} for x in results]
+    session.close()
+    return jsonify(output)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,17 +1,17 @@
-var url = "https://raw.githubusercontent.com/kealvarez/project3/main/resources/CleanFullTimeData.csv"
+var url = "http://127.0.0.1:5000/api/v1.0/all"
 
-var data = d3.csv(url, function(data) {
+var data = d3.json(url, function(data) {
     console.log(data);
 });
 
-d3.csv(url, function (err, rows){
+d3.json(url, function (err, rows){
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
     }
     var trace1 = {
         x: unpack(rows, 'id'),
-        y: unpack(rows, 'work_year'),
-        z: unpack(rows, 'salary_in_usd'),
+        y: unpack(rows, 'salary_in_usd'),
+        z: unpack(rows, 'job_title'),
         marker: {
             size: 2,
             color: unpack(rows, 'salary_in_usd'),
