646acd574b02db8e63aa804c63200a1f0637a491
Mark <markosowski2023@u.northwestern.edu>
2022-10-05 19:02:55 -0500

    sample plot

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,19 +1,36 @@
-var url = "https://raw.githubusercontent.com/kealvarez/project3/main/resources/FullTimeEmployeeData.csv"
+var url = "https://raw.githubusercontent.com/kealvarez/project3/main/resources/CleanFullTimeData.csv"
 
-var dataset = [];
-
-
-d3.csv(url, function(data) {
+var data = d3.csv(url, function(data) {
     console.log(data);
-})
+});
 
-
-d3.csv(url, function (err, rows) {
+d3.csv(url, function (err, rows){
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
     }
+    var trace1 = {
+        x: unpack(rows, 'id'),
+        y: unpack(rows, 'work_year'),
+        z: unpack(rows, 'salary_in_usd'),
+        marker: {
+            size: 2,
+            color: unpack(rows, 'salary_in_usd'),
+            colorscale: 'Greens',
+            line: {color: 'transparent'}
+        },
+        mode: 'markers',
+        type: 'scatter3d',
+        text: unpack(rows, 'country_name'),
+        hoverinfo: 'x+y+z+text',
+        showlegend: false
 
-    // we need to do data['company_location'].unique() in the ipynb and then loook up up the lat and long for each unique value and add coloums to the csv
-    var country = unpack(rows, 'company_location'),
-        remoteRatio = unpack(rows, 'remote_ratio')
-})
+    };
+    var data = [trace1];
+    var layout = {margin: {
+        l: 0,
+        r: 0,
+        b: 0,
+        t: 0
+    }};
+    Plotly.newPlot('Mydiv', data, layout, {showLink: false});
+});
